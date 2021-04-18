a6e9f56255860ddc9bfb6023a74afdd0a7523caa
Becki Floyd <beckifloyd@gmail.com>
2021-04-18 10:08:12 +0100

    games played score

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-// ## Task 5: Game Loop
+// ## Task 6: Scores
 
-// Now that we have a fully functioning game, our next step is to have it run as many times
-//as people would like to play without having to refresh the page.
-
-// Use a `while loop` and `confirm`.
-
-// This will be deemed as complete when a player can keep playing indefinitely and has the option
-//to stop playing after every round.
-
+// Keep track of how many games have been played, as well as the number of wins, losses, and draws.
+// This will be deemed as complete when this information is displayed after each round.
+//
 // Plan
-// - [x] Computer needs to select a new random number
-// - [x] Create a function for computer to pick a random choice 
-// - [x] Make random function run inside the computer wins function 
-//          Hughes - When calling a function in a function you have to define the return information in the new function as it is not assigning it to anything so you can't use it!
-//          Google - Called functions should go underneath
-// - [x] Option to play the game again
-// - [x] Use a while loop to play the game again.
-// - [x] Use a confirm to ask if the player would like to play again. If confirm = true play again.
-//           Simon - confirm is already true so can just use if answer === false
+// - [x] How many games have been played 
+        // - Create a variable for games played x
+        // - If score increases by 1 add 1 to games played
+// - [] How many player wins 
+        // - Create a variable for player wins
+        // - If score increases by 1 add 1 to player wins
+// - [] How many player losses
+        // - Create a variable for player losses
+        // - If score increases by 1 add 1 to player lossess
+// - [] How many draws
+        // - Create a variable for draw
+        // - Add code to alert draw
+        // - If draw increases by 1 add 1 to draw
+// - [] Display Information after each game
+
 
 let result = 0;
 let answer = true;
+let gamesPlayed = 0;
 
 function getWinner() {
  while (answer === true) {
+    gamesPlayed++;
+    console.log(gamesPlayed);
   let computerMove = computerPicks();
   let playerMove = prompt("Do you pick rock, paper or scissors?");
   if (playerMove === "paper" && computerMove === "rock") {
@@ -50,7 +54,7 @@ function getWinner() {
 
   answer = confirm("Do you want to play again?");
   if (answer === false) {
-   alert("Thank you for playing");
+   alert("Thank you for playing " + gamesPlayed + " games!");
   }
  }
 }
