e416788542d12a87630dabc8579586370c29f06c
Becki Floyd <beckifloyd@gmail.com>
2021-04-23 08:51:43 +0100

    Changed CSS to wrestling theme

--- main.js
+++ main.js
@@ -1,20 +1,166 @@
-// ## Task 8: Validation
+// ## Task 9: Style, Animation and User Experience
 
-// Create a username `input` field and use the username the player inputs in the game so that a player can see their name on the page when looking at where the scores are displayed.
+// Use CSS to add some style, flair, and animation to the playing experience on the page. Be creative! ✨ Keep in mind your user and make their experience as easy and enjoyable as possible.
 
-// To make it more uniform, restrict the number of characters a username can be to 10 or fewer.
 
-// This will be deemed as complete when the users cannot enter a username longer than 10 characters.
+// -[] Add media queries for larger screens
+// -[x] Increase input size in text box
+// -[x] Add who won 
+// -[x] Replace pictures with wrestlers
+// -[x] Change wording on HTML
+// -[x] Change wording on JS
+// -[x] Change background 
+// -[x] Change colour of table
+// -[x] Change colour of button
+// -[] Add sound on name click 
+// -[] Add gif in between rounds
+// -[] Change wording in the Win to famous quotes 
 
-// 🌟 BONUS: Make it so that valid usernames should only start with letters, not numbers or symbols.
-// 🌟 EXTRA BONUS: Make it so that the first letter of the username should be capitalised.
 
-//Plan
-//  -[x] Create an input field
-//  -[x] Make input replace player text
-//  -[x] Restrict username to be less than 10 characters
-//  -[x] Username should be capitalised
-//  -[ ] Make username only start with letters,  not numbers or symbols.
+
+// DOM Variables
+
+let result = 0;
+let answer = true;
+let gamesPlayed = 1;
+let playerWins = 1;
+let playerLosses = 1;
+let draw = 1;
+let playerMove;
+let computerMove;
+let displayComputerChoice = document.getElementById("computer-pick");
+
+// Player Clicks on Image which is stored in event
+// When player clicks on choice, Computer Picks their Random Choice
+
+function computerPicks() {
+ let choice = ["Wrestler", "Sumo", "Samurai"];
+
+ let random = Math.floor(Math.random() * choice.length);
+
+ computerChoice = (random, choice[random]);
+
+ let computerMove = computerChoice;
+
+ return computerMove;
+}
+
+// Compare Player Choice to Computer
+// If player wins increase You won text by 1
+// If player lost increase You lost text by 1
+// If player draw increase draw text by 1
+// Number of games increase
+
+// Rock
+
+let rockImage = document.querySelector("#rock");
+
+function playerRock(event) {
+ console.log("player picks rock");
+ console.log("computer picks " + computerMove);
+ playerMove = "Wrestler";
+ computerMove = computerPicks();
+ if (computerMove === "Wrestler") {
+  console.log("draw");
+  drawIncrease();
+ } else if (computerMove === "Samurai") {
+  console.log("player wins");
+  playerScoreIncrease();
+ } else if (computerMove === "Sumo") {
+  console.log("computer wins");
+  lossIncrease();
+ }
+
+ gamesScoreIncrease();
+ 
+}
+
+rockImage.addEventListener("click", playerRock);
+
+//paper
+
+let paperImage = document.querySelector("#paper");
+
+function playerPaper(event) {
+ console.log("player picks paper");
+ console.log("computer picks " + computerMove);
+ playerMove = "Sumo";
+ computerMove = computerPicks();
+ if (computerMove === "Sumo") {
+  console.log("draw");
+  drawIncrease();
+ } else if (computerMove === "Wrestler") {
+  console.log("player wins");
+  playerScoreIncrease();
+ } else if (computerMove === "Samurai") {
+  console.log("computer wins");
+  lossIncrease();
+ }
+
+ gamesScoreIncrease();
+ 
+}
+
+paperImage.addEventListener("click", playerPaper);
+
+//scissors
+
+let scissorsImage = document.querySelector("#scissors");
+
+function playerScissors(event) {
+ console.log("player picks scissors");
+ console.log("computer picks " + computerMove);
+ playerMove = "Samurai";
+ computerMove = computerPicks();
+ if (computerMove === "Samurai") {
+  console.log("draw");
+  drawIncrease();
+ } else if (computerMove === "Sumo") {
+  console.log("player wins");
+  playerScoreIncrease();
+ } else if (computerMove === "Wrestler") {
+  console.log("computer wins");
+  lossIncrease();
+ }
+
+ gamesScoreIncrease();
+
+}
+
+scissorsImage.addEventListener("click", playerScissors);
+
+// Scores
+
+function gamesScoreIncrease() {
+ let gamesScore = document.querySelector("#score-games");
+ result = gamesPlayed++;
+ gamesScore.innerText = result;
+}
+
+function playerScoreIncrease() {
+ let playerScore = document.querySelector("#score-won");
+ result = playerWins++;
+ playerScore.innerText = result;
+ displayComputerChoice.innerText = "You picked " + playerMove + "! The computer picked " + computerMove + "!" + "\nVICTORY IS YOURS!!!!!";
+}
+
+function drawIncrease() {
+ let drawScore = document.querySelector("#score-draw");
+ result = draw++;
+ drawScore.innerText = result;
+ displayComputerChoice.innerText = "You picked " + playerMove + "! The computer picked " + computerMove + "!" + "\nDRAW!!!!!";
+}
+
+function lossIncrease() {
+ let lostScore = document.querySelector("#score-lost");
+ result = playerLosses++;
+ lostScore.innerText = result;
+ displayComputerChoice.innerText = "You picked " + playerMove + "! The computer picked " + computerMove + "!" + "\nYOU HAVE BEEN DEFEATED!";
+}
+
+// When hit reset, change all numbers to 0
+
+let playAgain = document.querySelector("#play-again");
 
 // Create an input field
 // Make input replace player text
@@ -58,146 +204,9 @@ inputtedWord.addEventListener(
  (e) => (e.target.value = capitalise(e.target.value))
 );
 
+// -[] Add computer choice visually 
+// . make a place to display the text 
+// . make a function that displays computer choice
+// . add function to rock, paper, scissors
 
-
-
-
-// Make input replace player text
-
-// DOM Variables
-
-let result = 0;
-let answer = true;
-let gamesPlayed = 1;
-let playerWins = 1;
-let playerLosses = 1;
-let draw = 1;
-let playerMove;
-let computerMove;
-
-// Player Clicks on Image which is stored in event
-// When player clicks on choice, Computer Picks their Random Choice
-
-function computerPicks() {
- let choice = ["rock", "paper", "scissors"];
-
- let random = Math.floor(Math.random() * choice.length);
-
- computerChoice = (random, choice[random]);
-
- let computerMove = computerChoice;
-
- return computerMove;
-}
-
-// Compare Player Choice to Computer
-// If player wins increase You won text by 1
-// If player lost increase You lost text by 1
-// If player draw increase draw text by 1
-// Number of games increase
-
-// Rock
-
-let rockImage = document.querySelector("#rock");
-
-function playerRock(event) {
- console.log("player picks rock");
- console.log("computer picks " + computerMove);
- playerMove = "rock";
- computerMove = computerPicks();
- if (computerMove === "rock") {
-  console.log("draw");
-  drawIncrease();
- } else if (computerMove === "scissors") {
-  console.log("player wins");
-  playerScoreIncrease();
- } else if (computerMove === "paper") {
-  console.log("computer wins");
-  lossIncrease();
- }
-
- gamesScoreIncrease();
-}
-
-rockImage.addEventListener("click", playerRock);
-
-//paper
-
-let paperImage = document.querySelector("#paper");
-
-function playerPaper(event) {
- console.log("player picks paper");
- console.log("computer picks " + computerMove);
- playerMove = "paper";
- computerMove = computerPicks();
- if (computerMove === "paper") {
-  console.log("draw");
-  drawIncrease();
- } else if (computerMove === "rock") {
-  console.log("player wins");
-  playerScoreIncrease();
- } else if (computerMove === "scissors") {
-  console.log("computer wins");
-  lossIncrease();
- }
-
- gamesScoreIncrease();
-}
-
-paperImage.addEventListener("click", playerPaper);
-
-//scissors
-
-let scissorsImage = document.querySelector("#scissors");
-
-function playerScissors(event) {
- console.log("player picks scissors");
- console.log("computer picks " + computerMove);
- playerMove = "scissors";
- computerMove = computerPicks();
- if (computerMove === "scissors") {
-  console.log("draw");
-  drawIncrease();
- } else if (computerMove === "paper") {
-  console.log("player wins");
-  playerScoreIncrease();
- } else if (computerMove === "rock") {
-  console.log("computer wins");
-  lossIncrease();
- }
-
- gamesScoreIncrease();
-}
-
-scissorsImage.addEventListener("click", playerScissors);
-
-// Scores
-
-function gamesScoreIncrease() {
- let gamesScore = document.querySelector("#score-games");
- result = gamesPlayed++;
- gamesScore.innerText = result;
-}
-
-function playerScoreIncrease() {
- let playerScore = document.querySelector("#score-won");
- result = playerWins++;
- playerScore.innerText = result;
-}
-
-function drawIncrease() {
- let drawScore = document.querySelector("#score-draw");
- result = draw++;
- drawScore.innerText = result;
-}
-
-function lossIncrease() {
- let lostScore = document.querySelector("#score-lost");
- result = draw++;
- lostScore.innerText = result;
-}
-
-// When hit reset, change all numbers to 0
-
-let playAgain = document.querySelector("#play-again");
 
